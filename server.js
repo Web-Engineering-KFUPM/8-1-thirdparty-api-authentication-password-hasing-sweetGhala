0513a312e07aef5d69d6f3335fab64c5e44f688f
Ghala <you@example.com>
2026-04-20 11:38:33 +0300

    todo2

--- server.js
+++ server.js
@@ -245,7 +245,6 @@ app.get("/", (_req, res) => {
 // =========================
 app.post("/register", async (req, res) => {
   // Implement logic here based on the TODO 1.
-  app.post("/register", async (req, res) => {
     try {
       const { email, password } = req.body || {};
 
@@ -271,7 +270,7 @@ app.post("/register", async (req, res) => {
       console.error("Register error:", err);
       return res.status(500).json({ error: "Server error during register" });
     }
-  });
+
 });
 
 // =========================
@@ -279,6 +278,36 @@ app.post("/register", async (req, res) => {
 // =========================
 app.post("/login", async (req, res) => {
   // Implement logic here based on the TODO 2.
+    try {
+      const { email, password } = req.body || {};
+
+      if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+      }
+
+      const user = users.find((u) => u.email === email);
+
+      if (!user) {
+        return res.status(400).json({ error: "User not found" });
+      }
+
+      const match = await bcrypt.compare(password, user.passwordHash);
+
+      if (!match) {
+        return res.status(400).json({ error: "Wrong password" });
+      }
+
+      const token = jwt.sign(
+          { email },
+          JWT_SECRET,
+          { expiresIn: "1h" }
+      );
+
+      return res.json({ token });
+    } catch (err) {
+      console.error("Login error:", err);
+      return res.status(500).json({ error: "Server error during login" });
+    }
 });
 
 // =========================
